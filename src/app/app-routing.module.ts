950f0014cfc37024b3e73f849cddd5725c056f1b
Naveena-25 <naveena25.gurram@gmail.com>
2020-04-24 11:27:09 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path :'template-driven-forms',component : TemplateDrivenFormsComponent },
+  { path :'reactive-forms',component : ReactiveFormsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
